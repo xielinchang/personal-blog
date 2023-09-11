439d5cd1738db12cf195c66349ae82087443a3ea
谢林昌 <307001300@qq.com>
2023-09-11 23:35:49 +0800

    1

--- blog/vue-admin/src/views/blog/Aboutme.vue
+++ blog/vue-admin/src/views/blog/Aboutme.vue
@@ -2,8 +2,7 @@
   <div>
     <div class="aboutme block">
       <div class="label">详细信息:</div>
-      <div class="check-about-c">
-      </div>
+      <div class="check-about-c"></div>
       <div class="edit-aboutme-c">
         <div class="inputs">
           qq:
@@ -15,7 +14,7 @@
           ></my-input>
           email:
           <my-input
-          :disabled="editFlag"
+            :disabled="editFlag"
             v-model="aboutmeForm.email"
             class="input-item"
             :width="inputSize"
@@ -30,9 +29,9 @@
         </div>
         <div class="web-declare">
           <my-textarea
-          :disabled="editFlag"
+            :disabled="editFlag"
             v-model="aboutmeForm.web_declare"
-            style="margin-top: 10px;"
+            style="margin-top: 10px"
             label="网站申明:"
             width="400"
             placeholder="请输入内容"
@@ -43,84 +42,82 @@
         <div class="techniques">
           <span>技术栈:</span>
           <my-tags
-          v-if="!editFlag"
+            v-if="!editFlag"
             :value="techniques"
+            @update-tags="updateTechniques"
           ></my-tags>
           <ul v-else class="techniques-tags">
-          <li
-            v-for="item in techniques"
-            :key="item"
-          >
-            {{ item }}
-          </li>
-        </ul>
+            <li v-for="item in techniques" :key="item">
+              {{ item }}
+            </li>
+          </ul>
         </div>
         <div class="btns">
-            <my-button
-          class="edit-btn"
-          @click="editFlag=!editFlag"
-          :type="editFlag?'primary':'danger'"
-        >{{ editFlag?"修改":"取消" }}</my-button>
-        <my-button
-        v-if="!editFlag"
-          class="edit-btn"
-          @click="edit"
-          type="success"
-        >确认修改</my-button>
+          <my-button
+            class="edit-btn"
+            @click="editFlag = !editFlag"
+            :type="editFlag ? 'primary' : 'danger'"
+            >{{ editFlag ? "修改" : "取消" }}</my-button
+          >
+          <my-button
+            v-if="!editFlag"
+            class="edit-btn"
+            @click="edit"
+            type="success"
+            >确认修改</my-button
+          >
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-import { queryAboutme, updateAboutme } from '@/api/main/aboutme'
+import { queryAboutme, updateAboutme } from "@/api/main/aboutme";
 
 export default {
-data () {
+  data() {
     return {
-        prefix: process.env.VUE_APP_BASE_API,
-        inputSize: 300,
+      prefix: process.env.VUE_APP_BASE_API,
+      inputSize: 300,
       aboutmeForm: {
-        qq: '',
-        email: '',
-        phone: '',
-        techniques: '',
-        web_declare: ''
+        qq: "",
+        email: "",
+        phone: "",
+        techniques: "",
+        web_declare: "",
       },
-      editFlag:true,
+      editFlag: true,
       techniques: [],
-    }
-},
-mounted () {
-  this.initAboutme()  
-},
-methods:{
+    };
+  },
+  mounted() {
+    this.initAboutme();
+  },
+  methods: {
     edit() {
-    
-      this.aboutmeForm.techniques = this.techniques.join(',')
-      console.log(this.aboutmeForm);
-      updateAboutme(this.aboutmeForm).then(res => {
-        console.log(res)
-        this.initAboutme()
+      this.aboutmeForm.techniques = this.techniques.join(",");
+      updateAboutme(this.aboutmeForm).then((res) => {
+        this.initAboutme();
         this.$message({
-            message: '修改成功',
-          type: 'success'
-        })
-        this.editFlag=true
-      })
+          message: "修改成功",
+          type: "success",
+        });
+        this.editFlag = true;
+      });
+    },
+    updateTechniques(e) {
+      this.techniques = e;
     },
     initAboutme() {
-      queryAboutme().then(res => {
-        console.log(res);
-        this.aboutmeForm = res[0]
-        this.techniques = res[0].techniques.split(',')
-        console.log(res)
-      })
+      queryAboutme().then((res) => {
+        this.aboutmeForm = res[0];
+        this.techniques = res[0].techniques.split(",");
+      });
     },
-}
-}
+  },
+};
 </script>
 
 <style lang='scss' scoped>
-@import './scss/Aboutme.scss';
+@import "./scss/Aboutme.scss";
 </style>
